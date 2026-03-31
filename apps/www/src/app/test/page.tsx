89daace5a677084f93e3219541944fdf6013e683
Evgeniy Chernyshenko <yevgeniyche@gmail.com>
2026-03-31 18:09:07 +0300

    инициализировать проект

---
+++ apps/www/src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Test() {
+  return (
+    <>
+      <h1>WWW test page</h1>
+
+      <Link href="/app/test">App test</Link>
+    </>
+  );
+}
